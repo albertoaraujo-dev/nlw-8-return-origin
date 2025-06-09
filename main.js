81a422f9c2ed22af06dd83489db2d9f1f5f6b4ff
Alberto Araujo <albertoaraujo.dev@gmail.com>
2025-06-09 13:53:24 -0300

    refactor: layout, text, images, colors

--- main.js
+++ main.js
@@ -4,91 +4,95 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 
 function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
-  activateMenuAtCurrentSection(home)
-  activateMenuAtCurrentSection(services)
-  activateMenuAtCurrentSection(about)
-  activateMenuAtCurrentSection(contact)
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
 
 function activateMenuAtCurrentSection(section) {
-  //cálculo para verificar onde o scroll está na viewport
-  const targetLine = scrollY + innerHeight / 2
+    //cálculo para verificar onde o scroll está na viewport
+    const targetLine = scrollY + innerHeight / 2
 
-  const sectionTop = section.offsetTop
-  const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
 
-  const sectionEndsAt = sectionTop + sectionHeight
-  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-  //está dentro da sessão
-  const sectionBoundaries =
-    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+    //está dentro da sessão
+    const sectionBoundaries =
+        sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
-  const sectionId = section.getAttribute('id')
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
-  menuElement.classList.remove('active')
-  if (sectionBoundaries) {
-    menuElement.classList.add('active')
-  }
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
-  if (scrollY > 0) {
-    navigationHeader.classList.add('scroll')
-  } else {
-    navigationHeader.classList.remove('scroll')
-  }
+    if (scrollY > 0) {
+        navigationHeader.classList.add('scroll')
+        logoImage.setAttribute('src', './assets/logo.png')
+    } else {
+        navigationHeader.classList.remove('scroll')
+        logoImage.setAttribute('src', './assets/logo-dark.png')
+    }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 400) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
-  function isMobile() {
-    const minWidth = 768; // Minimum width for desktop devices
-    return window.innerWidth < minWidth || screen.width < minWidth;
-  }
-  
-  if (isMobile()) {
-    if (scrollY > 4000) {
-      backToTopButton.classList.add('footerColor')
+    if (scrollY > 400) {
+        backToTopButton.classList.add('show')
     } else {
-      backToTopButton.classList.remove('footerColor')
+        backToTopButton.classList.remove('show')
     }
-  } else {
-    if (scrollY > 2000) {
-      backToTopButton.classList.add('footerColor')
+    function isMobile() {
+        const minWidth = 768; // Minimum width for desktop devices
+        return window.innerWidth < minWidth || screen.width < minWidth;
+    }
+
+    if (isMobile()) {
+        if (scrollY > 4000) {
+            backToTopButton.classList.add('footerColor')
+        } else {
+            backToTopButton.classList.remove('footerColor')
+        }
     } else {
-      backToTopButton.classList.remove('footerColor')
+        if (scrollY > 2000) {
+            backToTopButton.classList.add('footerColor')
+        } else {
+            backToTopButton.classList.remove('footerColor')
+        }
     }
-  } 
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+    document.body.classList.add('menu-expanded')
+    logoImage.setAttribute('src', './assets/logo.png')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+    document.body.classList.remove('menu-expanded')
+    logoImage.setAttribute('src', './assets/logo-dark.png')
 }
 
 ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
 }).reveal(`
-  #home,
-  #home img,
-  #home .stats,
-  #services,
-  #services header,
-  #services .card,
-  #about,
-  #about header,
-  #about content`)
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about content`)
